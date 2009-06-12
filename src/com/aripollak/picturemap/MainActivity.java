4f791f4bb1987bf57f049d870ee0f326d583bc29
Ari Pollak <ari@ebnj.net>
2009-06-11 23:32:08 -0400

    Working map view with picture->point

--- src/com/aripollak/picturemap/MainActivity.java
+++ src/com/aripollak/picturemap/MainActivity.java
@@ -1,18 +1,32 @@
 package com.aripollak.picturemap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.MediaStore.Images;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class MainActivity extends MapActivity {
-
+	
+	MapView mMapView;
+	List<Overlay> mMapOverlays;
+	Drawable mDrawable;
+	ImageOverlay mItemizedOverlay;
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,10 +36,17 @@ public class MainActivity extends MapActivity {
         Button button = (Button)findViewById(R.id.choosepic);
         button.setOnClickListener(mGetImageListener);
         
-        MapView map = (MapView) findViewById(R.id.map);
-        map.setBuiltInZoomControls(true);
+        mMapView = (MapView) findViewById(R.id.map);
+        mMapView.setBuiltInZoomControls(true);
+        
+        mMapOverlays = mMapView.getOverlays();
+        mDrawable = this.getResources().getDrawable(
+        				android.R.drawable.ic_menu_myplaces);
+        mItemizedOverlay = new ImageOverlay(mDrawable);
+    	mMapOverlays.add(mItemizedOverlay);
     }
     
+    /** Clicked on Choose Picture button */
     private final OnClickListener mGetImageListener = new OnClickListener() {
     	public void onClick(View v) {
     		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);              
@@ -33,16 +54,21 @@ public class MainActivity extends MapActivity {
             // TODO: specify a content uri for the camera bucket by using a ContentProvider?
             // TODO: Just get called from gallery for now? 
             intent.setType("image/*");
-            startActivityForResult(Intent.createChooser(intent, getString(R.string.select_image)), 0);
+            startActivityForResult(Intent.createChooser(
+            		intent, getString(R.string.select_image)), 0);
     	}
     };
 
+    /** Got back a result from the picture chooser */
+    // TODO: load all images with location, and let people search for
+    //       stuff by date/picture?
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-    	Log.d(this.getLocalClassName(), "onActivityResult Received " + intent);
     	if (intent == null)
     		return;
+    	
     	Cursor cursor = managedQuery(intent.getData(), null, null, null, null);
+    	int titleColumn = cursor.getColumnIndexOrThrow(Images.Media.TITLE);
     	int bucketNameColumn = cursor.getColumnIndexOrThrow(Images.Media.BUCKET_DISPLAY_NAME);
     	int bucketIdColumn = cursor.getColumnIndexOrThrow(Images.Media.BUCKET_ID);
     	int longitudeColumn = cursor.getColumnIndexOrThrow(Images.Media.LONGITUDE);
@@ -56,8 +82,45 @@ public class MainActivity extends MapActivity {
     		stuff += " " + cursor.getString(dataColumn);
     		Log.i(this.getLocalClassName(), stuff);
     	}
+    	
+    	// TODO: set item's marker as the picture
+    	int lat = (int) (cursor.getDouble(latitudeColumn) * 1E6);
+    	int lon = (int) (cursor.getDouble(longitudeColumn) * 1E6);
+    	GeoPoint point = new GeoPoint(lat, lon);
+    	OverlayItem item = new OverlayItem(point, cursor.getString(titleColumn), "");
+    	mItemizedOverlay.addOverlay(item);
+    	mMapView.getController().animateTo(point);
     }
     
     @Override
     protected boolean isRouteDisplayed() { return false; }
+    
+    
+	private class ImageOverlay extends ItemizedOverlay<OverlayItem> {
+
+		private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+		
+		public ImageOverlay(Drawable defaultMarker) {
+			super(boundCenterBottom(defaultMarker));
+			populate();
+		}
+		
+		public void addOverlay(OverlayItem item) {
+		    mOverlays.add(item);
+		    populate();
+		}
+
+		@Override
+		protected OverlayItem createItem(int i) {
+			return mOverlays.get(i);
+		}
+
+
+		@Override
+		public int size() {
+			return mOverlays.size();
+		}
+
+	}
+   
 }
